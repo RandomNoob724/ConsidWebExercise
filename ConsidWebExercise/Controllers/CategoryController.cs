991bce646dbf26bcd37e53434154ac7bc7aa2c25
Emil Persson <emilper1999@gmail.com>
2021-04-23 22:59:58 +0200

    Create Categories functionality implemented in Category Controller

---
+++ ConsidWebExercise/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using ConsidWebExercise.Data;
+using ConsidWebExercise.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsidWebExercise.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: Category/Index
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Categories;
+            return View(categoryList);
+        }
+
+        // GET: Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category categoryObj)
+        {
+            _db.Add(categoryObj);
+            _db.SaveChanges();
+            return Redirect("Index");
+        }
+    }
+}
